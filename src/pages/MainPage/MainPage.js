7e0c052971dff83e899e0be71d0ee92d7c02e395
Elco Garcia <garciaelco18@gmail.com>
2020-03-15 21:03:35 -0500

    added thumbnails

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TitlePage from '../../components/TitlePage/TitlePage';
+import ThumbNails from '../../components/Thumbnails/ThumbNails';
 
 const mainPage = () => {
 	return (
 		<div>
 			<TitlePage />
+			<ThumbNails />
 		</div>
 	);
 };
