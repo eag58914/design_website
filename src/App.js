c60e716bfe5c8766d9510cbfe13c1800acf1dc92
Elco Garcia <garciaelco18@gmail.com>
2020-04-05 18:43:57 -0500

    business logic for showing sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 import MainPage from '../src/pages/MainPage/MainPage';
 import Toolbar from './components/Toolbar/Toolbar';
@@ -6,20 +6,38 @@ import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
 import './App.css';
 
-const App = () => {
-	return (
-		<div style={{ height: '100%' }}>
-			<Toolbar />
-			<SideDrawer />
-			<Backdrop />
-			<p>This is the page content</p>
-			<Router>
-				<Switch>
-					<Route path="/main" render={() => <MainPage />} />
-				</Switch>
-			</Router>
-		</div>
-	);
-};
+class App extends Component {
+	state = {
+		sideDrawerOpen: false
+	};
+	drawerToggleClickHandler = () => {
+		this.setState((prevState) => {
+			return { SideDrawerOpen: !prevState.SideDrawerOpen };
+		});
+	};
+	backdropClickHandler = () => {
+		this.setState({ SideDrawerOpen: false });
+	};
+	render() {
+		let backDrop;
+
+		if (this.state.SideDrawerOpen) {
+			backDrop = <Backdrop click={this.backdropClickHandler} />;
+		}
+		return (
+			<div style={{ height: '100%' }}>
+				<Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+				<SideDrawer show={this.state.SideDrawerOpen} />
+				{backDrop}
+				<p>This is the page content</p>
+				<Router>
+					<Switch>
+						<Route path="/main" render={() => <MainPage />} />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
+}
 
 export default App;
