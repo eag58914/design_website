5edfca4f690937fe1a574c4a9eb2c595e6f4a9d5
Elco Garcia <garciaelco18@gmail.com>
2020-04-05 15:29:24 -0500

    imported components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 import MainPage from '../src/pages/MainPage/MainPage';
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 import './App.css';
 
 const App = () => {
 	return (
-		<div>
+		<div style={{ height: '100%' }}>
+			<Toolbar />
+			<SideDrawer />
+			<Backdrop />
+			<p>This is the page content</p>
 			<Router>
 				<Switch>
 					<Route path="/main" render={() => <MainPage />} />
