45f62f8769779a6f56b982859bc91509144f5545
Elco Garcia <garciaelco18@gmail.com>
2020-04-05 18:44:46 -0500

    sidebar css

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './SideDrawer.css';
-const sideDrawer = (props) => (
-	<nav className="side-drawer">
-		<ul>
-			<li>Link 3 </li>
-			<li>Link 4</li>
-		</ul>
-	</nav>
-);
+
+const sideDrawer = (props) => {
+	let drawerClasses = 'side-drawer';
+	if (props.show) {
+		drawerClasses = 'side-drawer open';
+	}
+	return (
+		<nav className={drawerClasses}>
+			<ul>
+				<li>Link 3 </li>
+				<li>Link 4</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default sideDrawer;
