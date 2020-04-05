987d5878ed7b620bb87d67f9985163dd80807b80
Elco Garcia <garciaelco18@gmail.com>
2020-04-05 15:30:36 -0500

    sidedrawer js and css

---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideDrawer.css';
+const sideDrawer = (props) => (
+	<nav className="side-drawer">
+		<ul>
+			<li>Link 3 </li>
+			<li>Link 4</li>
+		</ul>
+	</nav>
+);
+
+export default sideDrawer;
