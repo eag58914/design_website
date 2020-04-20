e78e0f83371613c0560614664c27c3b62509b165
Elco Garcia <garciaelco18@gmail.com>
2020-04-19 22:35:43 -0500

    minor changes

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -4,49 +4,55 @@ const Thumbnails = () => {
 	return (
 		<div className="thumbnails-containers">
 			<ul>
-				<li>
+				<li className="thumbnail-li">
 					<img
+						className="thumbnail-image"
 						src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05466_kwlv0n.jpg"
 						alt="toyota"
 					/>
 				</li>
-				<li>
+				<li className="thumbnail-li">
 					<img
+						className="thumbnail-image"
 						src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05621_zgtcco.jpg"
 						alt="Interesting living room light through a window"
 						loading="lazy"
 					/>
 				</li>
-				<li>
+				<li className="thumbnail-li">
 					<img
+						className="thumbnail-image"
 						src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05513_gfbiwi.jpg"
 						alt="Sara on a red bike"
 						loading="lazy"
 					/>
 				</li>
-				<li>
+				<li className="thumbnail-li">
 					<img
+						className="thumbnail-image"
 						src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05588_nb0dma.jpg"
 						alt="XOXO venue in between talks"
 						loading="lazy"
 					/>
 				</li>
-				<li>
+				<li className="thumbnail-li">
 					<img
+						className="thumbnail-image"
 						src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05459_ziuomy.jpg"
 						alt="Trees lit by green light during dusk"
 						loading="lazy"
 					/>
 				</li>
-				<li>
+				<li className="thumbnail-li">
 					<img
+						className="thumbnail-image"
 						src="https://res.cloudinary.com/css-tricks/image/upload/f_auto,q_auto/v1568814785/photostream-photos/DSC05586_oj8jfo.jpg"
 						alt="Portrait of Justin Pervorse"
 						loading="lazy"
 					/>
 				</li>
 			</ul>
-			<li />
+			<li className="thumbnail-li" />
 		</div>
 	);
 };
