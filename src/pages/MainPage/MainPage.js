4b4a4c4720605bf227a7d0b64fda95b71b5d8c87
Elco Garcia <garciaelco18@gmail.com>
2020-04-19 21:36:08 -0500

    mainpage changes

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Thumbnails from '../../components/Thumbnail/Thumbnail';
+import './MainPage.css';
+
+const MainPage = () => {
+	return (
+		<div className="container">
+			<Thumbnails />
+		</div>
+	);
+};
+
+export default MainPage;
