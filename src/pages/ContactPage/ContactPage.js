41cc13f96ff82ed606dd8946aec2f30791bcf088
Elco Garcia <garciaelco18@gmail.com>
2020-05-02 19:46:21 -0500

    started contact page

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import mainImage from '../../images/Grafiti.png';
+import './ContactPage.css';
 
 const ContactPage = (props) => {
-	return <div>Contact Page</div>;
+	return (
+		<div>
+			<img src={mainImage} alt="contact" className="contact-image" />
+			<p className="contact-text">
+				Hi! My name is Amanda Lorraine and I’m a grad student at The University of Texas at Austin. I love dogs,
+				I’m an introvert — I only leave the house if there’s promise of alcohol or architecture. Resume
+				attached. Referrals by request only. Thanks
+			</p>
+		</div>
+	);
 };
 
 export default ContactPage;
