85410b32b16f13766185fda6e2b4098d0e89ea52
Elco Garcia <garciaelco18@gmail.com>
2020-05-21 13:39:24 -0500

    favicon added

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(function(req, res, next) {
 	next();
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
