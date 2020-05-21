0ff3f3f69fdbf23f91db732a50c4a66967b7686f
Elco Garcia <garciaelco18@gmail.com>
2020-05-21 13:38:08 -0500

    not needed

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.use(function(req, res, next) {
 	next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
