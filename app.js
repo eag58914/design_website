66e0e7e392a3b44d1d990c85dec2a5b09c10444a
Elco Garcia <garciaelco18@gmail.com>
2020-06-23 21:52:32 -0500

    chnged headers, allowing acces for testing

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 
 var app = express();
 
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Origin', 'localhost:3000');
+	res.header('Access-Control-Allow-Headers', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
+app.use(cors());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
