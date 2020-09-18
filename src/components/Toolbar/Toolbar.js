f4355c729662c6bdc4292e72d87b27b1eb4f801b
Elco Garcia <garciaelco18@gmail.com>
2020-09-17 21:30:34 -0500

    imported logo

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Toolbar.css';
 import {Link } from 'react-router-dom'
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
-import logo from '../images/ALH.jpg'
+
 
 const toolbar = (props) => (
 	<header className="toolbar">
